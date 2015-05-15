d8ed60ab9155993f8d6872712ffecdd2965485c0
Joe Kent <thedeadlybutter@gmail.com>
2015-05-15 11:43:42 -0400

    Updates campaign query to also use Instagram and search per term

--- queries/campaigns.js
+++ queries/campaigns.js
@@ -1,26 +1,39 @@
+var posts = [];
+var totalTerms = 0;
+var termsSearched = 0;
+
 this.run = function(app, finished) {
   global.request
    .get("http://www.dosomething.org/api/v1/campaigns.json?parameters[is_staff_pick]=1")
    .end(function(err, res) {
      var data = res.body;
      var searchTerm = "";
+     totalTerms = data.length * 2;
      data.forEach(function(element, index, array) {
-      searchTerm += '"' + element.title + '"' + ' OR ';
-     });
-     global.twitterClient.get('search/tweets', {q: searchTerm, count: 100}, function(error, tweets, response){
-       var posts = [];
-       if(!error) {
-         tweets.statuses.forEach(function(element, index, array){
-           var post = global.createPostObjectFromTweet(element);
-           posts.push(post);
-         });
-       }
-       finished(posts);
-     });
-   });
+      searchTerm = element.title.replace(/[\W_]+/g,"");
+      global.instagramClient.tag_media_recent(searchTerm.replace(/ /g, ""), function(err, result, remaining, limit) {
+        result.forEach(function(element, index, array) {
+          posts.push(global.createPostObjectFromInstagram(element));
+        });
+        termsSearched++;
+        searchFinished(finished);
+      });
+      global.twitterClient.get('search/tweets', {q: '"' + searchTerm + '"', count: 100}, function(error, tweets, response) {
+        if(!error) {
+          tweets.statuses.forEach(function(element, index, array){
+            var post = global.createPostObjectFromTweet(element);
+            posts.push(post);
+          });
+        }
+        termsSearched++;
+        searchFinished(finished);
+      });
+    });
+  });
 }
 
-//Add in the data for Each a call to the instagram API
-//After all of those are done running call the Twitter
-//client and have it search the entire query string
-//Call finish with all of the posts.
+function searchFinished(queryFinish) {
+ if(termsSearched >= totalTerms) {
+   queryFinish(posts);
+ }
+}
